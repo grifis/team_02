d15601bb50feab9e6fe8a33ec45238fb8ca42855
bashi <bashi57.d@gmail.com>
2021-12-26 20:52:17 +0900

    editted

--- app/gohan/resources/views/main/index.blade.php
+++ app/gohan/resources/views/main/index.blade.php
@@ -8,8 +8,47 @@
   <link rel='stylesheet' href="css/gohan.css">
 </head>
 <body>
-  <h1>見えてますか？</h1>
-  <button onClick="return test_func();">ここを押してね</button>
+  <header>
+    <div class="header-container">
+      <h1>シュミレーション名</h1>
+    </div>
+
+  </header>
+
+  <body>
+    <div class="body-container">
+      <div class="side">
+        <button class="fruit">果物</button>
+        <button class="vegetables">野菜</button>
+        <button class="fish">魚</button>
+        <button class="foods">食べ物</button>
+      </div>
+
+      <div class="main">
+        <p class="fukidashi">体重を入力してね!!!!</p>
+        <form mathod="POST" id="weight" accept-charset="UTF-8"	action="/js/gohan.js">
+          <input type="number"><p>kg</p><input type="submit" value="決定">
+        </form>
+
+        <p>OOOで表すと、、、</p>
+
+        <div class="result">
+          <p>あなたの体重は、<br>OOOがXXX個分です。</p>
+
+        </div>
+      </div>
+
+    </div>
+    
+  </body>
+
+  <footer>
+    <div class="footer-container">
+
+    </div>
+
+  </footer>
+  <!-- <button onClick="return test_func();">ここを押してね</button> -->
 </body>
 <script src="js/gohan.js"></script>
 </html>
